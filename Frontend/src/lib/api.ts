8ee9f2239404cea7b5ccf1bd2547c193f51d11ee
Jayaveer <ramanadhamjayaveer@mictech.edu.in>
2026-08-05 15:10:38 +0530

    updated-api key

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ export const API_URL =
   import.meta.env.VITE_API_URL ||
   (window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://aotms-lms-new.onrender.com/api");
+    : "https://aotms-lms-dm0s.onrender.com/api");
 
 export const fetchWithAuth = async <T = unknown>(
   url: string,
